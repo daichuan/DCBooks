8beec7847315fbff127484739d23c1a4b5fad207
戴川 <1041936247@qq.com>
2018-03-13 13:58:33 +0800

    Initial Commit

---
+++ DCBooks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DCBooks
+//
+//  Created by cheyr on 2018/3/13.
+//  Copyright © 2018年 cheyr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DCBooks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DCBooks
+//
+//  Created by cheyr on 2018/3/13.
+//  Copyright © 2018年 cheyr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
